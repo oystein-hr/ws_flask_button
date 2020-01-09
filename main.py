0cf3a1f4c07009e9a6e47b9ba8aa62ef4edcf600
Øystein Hofvind-Rom <oystein@hofvind-rom.com>
2020-01-09 20:47:17 +0100

    Project abandoned, see readme.md

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
+from flask import Flask, send_file
+import websockets
+import webbrowser
+import asyncio
+import datetime
+
+
+# Websocket server loop
+async def time(websocket, path):    # ws_handler requires 2 arguments, path not used in this code
+    while True:
+        try:
+            # Get current time, isoformat() makes it a string in ISO 8601 format
+            now = datetime.datetime.now().time().isoformat(timespec='seconds')
+
+            await websocket.send(now)       # Send current time to GUI
+            await asyncio.sleep(1)          # Wait one second before running loop again
+
+        # If GUI is closed, exit the while loop
+        except websockets.ConnectionClosed:
+            break
+
+    # Stop the asyncio event loop - closing the program
+    asyncio.get_event_loop().stop()
+
 
 def main():
-    pass
+    app = Flask(__name__)
+
+    start_server = websockets.serve(time, "127.0.0.1", 5001)
+
+    # (mac os) Get current path and open the gui in default web browser
+    webbrowser.open_new_tab('http://127.0.0.1:5000')
+
+    # Start the websocket server
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_until_complete(app.run())
+
+    # Keep the program running
+    asyncio.get_event_loop().run_forever()
+
+    @app.route("/")
+    def hello():
+        return send_file('front-end/index.html')
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,7 +1,14 @@
 # Minimal Websocket & Flask app
-Status: Work in progress  
-Runtime environment: macOS, Python 3.8.1
+**Status:** Abandoned  
+**Runtime environment:** macOS, Python 3.8.1
 
 ## About
-Learning to use HTML/CSS/JS as front-end for Python apps, this mini project uses Websocket 
-and Flask as back-end.
+Learning to use HTML/CSS/JS as front-end for Python apps, this mini project tries to use Websocket 
+and Flask as back-end.  
+
+Turns out that it will most likely not work like I imagined, so I am abandoning this mini project.
+There are other socket io packages that works with Flask, but for what I want to use it as it
+will just make the apps to complicated. One of the reasons I wanted to try implement Flask 
+was to use it as a shortcut - so I could just point to localhost instead of a file in the
+browser, and avoid cross-platform issues with the file path. But I think it is better to 
+just make a method to handle cross-platform file paths when needed. 
